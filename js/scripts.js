82233ec1626600328dd5a935a9bc38823a30e096
Dzmitriy <dmitriu355@gmail.com>
2022-02-01 23:21:03 +0300

    path 2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const usersData = [];
+let usersData = [];
 
 const signUpButton = document.querySelector('input[value="Sign Up"]');
 const loginButton = document.querySelector('input[value=Login]');
@@ -11,17 +11,26 @@ const tryAgain = document.querySelector('.try__again')
 
 
 const regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-const regPassword = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
+
+// const regPassword = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
+
+if (localStorage.getItem('usersData')) {
+   usersData = JSON.parse(localStorage.getItem('usersData'));
+}
 
 
-signUp.addEventListener("click", function () {
+//=====================SignUp===========================//
+
+signUp.addEventListener("click", function () {    // Don't have acc? => Go to Sign in Form
    signUp.classList.add('hide');
    loginButton.classList.add('hide');
    userName.classList.remove('hide');
    signUpButton.classList.remove('hide');
    formTitle.innerHTML = 'Sign Up'
+   userName.value = email.value = password.value = '';
 })
 
+//=====================SignUpButton===========================//
 
 
 signUpButton.addEventListener("click", function () {
@@ -30,15 +39,21 @@ signUpButton.addEventListener("click", function () {
       Email: email.value,
       Password: password.value
    }
-   if (
+   if (                                         // If user don't put text in some area
       userName.value == '' ||
       email.value == '' ||
-      password.value == ''
+      password.value == '' ||
+      !regEmail.test(email.value)              // If email is wrong!
    ) {
       return;
+   } else if (checkEmailAndLogin() === true) {  // If username or password is already used
+      callBack('This username or password is already used');
+      tryAgain.classList.remove('hide');
    } else {
+      console.log(checkEmailAndLogin())       // Push to arr and save in localStorage   
       usersData.push(newUser);
-      userName.value = email.value = password.value = '';
+      localStorage.setItem('usersData', JSON.stringify(usersData));
+      userName.value = '';
       signUp.classList.remove('hide');
       loginButton.classList.remove('hide');
       userName.classList.add('hide');
@@ -47,64 +62,44 @@ signUpButton.addEventListener("click", function () {
    }
 })
 
+//=====================LoginButton===========================//
+
 loginButton.addEventListener("click", function (event) {
+
    event.preventDefault();
+
    if (usersData.length === 0) {
-      email.value = password.value = '';
-      signUp.classList.add('hide');
-      loginButton.classList.add('hide');
-      email.classList.add('hide');
-      password.classList.add('hide');
+      console.log(usersData.length)
+      callBack('Invalid Email or Password');
       tryAgain.classList.remove('hide');
-      formTitle.innerHTML = 'Invalid email or password'
-      formTitle.style.cssText = `
-      font-size: 26px;
-      font-weight: 300;
-      margin-bottom: 0px;
-      letter-spacing: 3px;
-      `
    }
 
-   if (
+   if (                              // If user don't put text in some area
       email.value == '' ||
-      password.value == ''
+      password.value == '' ||
+      (!regEmail.test(email.value))   // If email is wrong!
    ) {
       return;
-   } else { }
-   usersData.forEach(function (element) {
+   }
+
+   let find = false;
+   usersData.forEach(function (element) {    // Search this email and password ind arr
       if (
          email.value == element.Email &
          password.value == element.Password
       ) {
-         email.value = password.value = '';
-         signUp.classList.add('hide');
-         loginButton.classList.add('hide');
-         email.classList.add('hide');
-         password.classList.add('hide');
-         formTitle.innerHTML = 'Login successfully'
-         formTitle.style.cssText = `
-         font-size: 26px;
-         font-weight: 300;
-         margin-bottom: 0px;
-         letter-spacing: 3px;
-         `
-      } else {
-         email.value = password.value = '';
-         signUp.classList.add('hide');
-         loginButton.classList.add('hide');
-         email.classList.add('hide');
-         password.classList.add('hide');
-         tryAgain.classList.remove('hide');
-         formTitle.innerHTML = 'Invalid email or password'
-         formTitle.style.cssText = `
-         font-size: 26px;
-         font-weight: 300;
-         margin-bottom: 0px;
-         letter-spacing: 3px;
-         `
+         callBack('Login Successfully');
+         return find = true;
       }
    })
+   if (find === false) {
+      callBack('Invalid Email or Password');
+      tryAgain.classList.remove('hide');
+   }
 })
+
+//=====================TryAgain===========================//
+
 
 tryAgain.addEventListener("click", function () {
    signUp.classList.remove('hide');
@@ -122,3 +117,34 @@ tryAgain.addEventListener("click", function () {
    margin-bottom: 40px;
    `
 })
+
+function callBack(message) {
+   userName.classList.add('hide');
+   signUpButton.classList.add('hide');
+   signUp.classList.add('hide');
+   loginButton.classList.add('hide');
+   email.classList.add('hide');
+   password.classList.add('hide');
+   formTitle.innerHTML = message;
+   formTitle.style.cssText = `
+   font-size: 26px;
+   font-weight: 300;
+   margin-bottom: 0px;
+   letter-spacing: 3px;
+   `
+}
+
+
+function checkEmailAndLogin() {
+   let check = false;
+   usersData.forEach(function (element) {
+      if (
+         email.value == element.Email ||
+         userName.value == element.NameSurname
+      ) {
+         check = true;
+         return;
+      }
+   })
+   return check;
+}
